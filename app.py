2e73d2c45d3722eceb0821d3a0a5550a3fa0e22f
yan-vei <yana.veitsman@yandex.kz>
2023-07-03 16:06:38 +0600

    setup models for chat

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+import os
 from flask_socketio import SocketIO
 
-
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['SQLALCHEMY_DATABASE_URI']
+
+db = SQLAlchemy(app)
 socketio = SocketIO(app)
 
 
 if __name__ == '__main__':
+    db.init_app(app)
     socketio.run(app, debug=True)
